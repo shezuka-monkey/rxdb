3837ff74bd67d6dece9b2765bc0652f2f3feaf59
Bar <bar.isyo@gmail.com>
2019-09-20 08:12:38 +0300

    Update attachments.js

--- dist/es/plugins/attachments.js
+++ dist/es/plugins/attachments.js
@@ -45,7 +45,8 @@ export var blobBufferUtil = {
       console.log("type", type);
       console.log("data", data);
       blobBuffer = new Blob([data.data], {
-        type: type
+        //type: type
+        type: data.type
       });
       console.log("blobBuffer", blobBuffer);
         
